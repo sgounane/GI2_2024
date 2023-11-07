2fcce6b9a506cdc582d758541f78c668f8b53348
gounane <gounane.said@gmail.com>
2023-11-07 09:40:41 +0100

    Page statique

---
+++ TPs/TP4/hanouti/index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hnouti</title>
+    <link rel="stylesheet" href="styles/style.css">
+</head>
+<body>
+    <header>
+        <h1> Hanouti</h1>
+    </header>
+    <section>
+        <article> 
+            <div class="article">
+                <img src="images/1.jpg" alt="" class="prodimg">
+                <div class="titre">Linovo</div>
+                <div class="prix">3456DH</div>
+                <a href="add.php" class="add"></a>
+            </div>
+            <div class="article">
+                <img src="images/1.jpg" alt="" class="prodimg">
+                <div class="titre">Linovo</div>
+                <div class="prix">3456DH</div>
+                <a href="add.php" class="add"></a>
+            </div>
+            <div class="article">
+                <img src="images/1.jpg" alt="" class="prodimg">
+                <div class="titre">Linovo</div>
+                <div class="prix">3456DH</div>
+                <a href="add.php"  class="add"></a>
+            </div>
+            <div class="article">
+                <img src="images/1.jpg" alt="" class="prodimg">
+                <div class="titre">Linovo</div>
+                <div class="prix">3456DH</div>
+                <a href="add.php"  class="add"></a>
+            </div>
+        </article>
+        <aside> 
+            <form action="search.php" method="POST" class="searchefrm">
+                <input type="text" >
+                <input type="submit" value="Search" name="search">
+            </form>
+            <div class="title">Total: 234DH</div>
+            <div class="element">
+                <img src="images/1.jpg" alt="" class="prodimgmin">
+                <p>Linovo</p>
+                <p>3</p>
+                <a href="delete.php"  class="delete"></a>
+            </div>
+            <div class="element">
+                <img src="images/1.jpg" alt="" class="prodimgmin">
+                <p>Linovo</p>
+                <p>3</p>
+                <a href="delete.php"  class="delete"></a>
+            </div>
+            <div class="element">
+                <img src="images/1.jpg" alt="" class="prodimgmin">
+                <div>Linovo thinkpad</div>
+                <div>3</div>
+                <a href="delete.php"  class="delete"></a>
+            </div>
+        </aside>
+    </section>
+    <footer>Footer</footer>
+</body>
+</html>

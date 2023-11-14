6c168b756f7446747eb1057090f5ebc61a2a318d
gounane <gounane.said@gmail.com>
2023-11-14 10:07:32 +0100

    refactor

--- TPs/TP5-db/index.php
+++ TPs/TP5-db/index.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
 $path=$_SERVER["REQUEST_URI"];
+$basedir="";
 switch($path){
-case "/": header("Location:views/home.php");break;
-case "/add": header("Location:views/prodForm.php");break;
-case "/login": header("Location:views/login.php");break;
-case "/logout": header("Location:controllers/logout.php");break;
+case $basedir."/": header("Location:views/home.php");break;
+case $basedir."/add": header("Location:views/prodForm.php");break;
+case $basedir."/login": header("Location:views/login.php");break;
+case $basedir."/logout": header("Location:controllers/logout.php");break;
 
 default: header("Location:views/notfound.php");
     
